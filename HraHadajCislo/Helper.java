290b8778b0a9c8c59244e29d22f4b20dbb29eb8a
Molabe <153329619+Molabe@users.noreply.github.com>
2024-04-18 13:31:02 +0200

    Pridanie hry

---
+++ HraHadajCislo/Helper.java
@@ -0,0 +1,58 @@
+package HraHadajCislo;
+
+import java.util.Scanner;
+
+public class Helper {
+
+    public void hra(int bot) {
+
+        int hrac;
+
+        System.out.println("Zadaj cislo pre start hry: ");
+        Scanner sc = new Scanner(System.in);
+
+        hrac = sc.nextInt();
+
+        int pocetPokus = 3;
+
+        while (pocetPokus >= 0) {
+            if (hrac == bot) { /* Vyhlasi vyhru a program sa vypne */
+                System.out.println("Vyhral si");
+                break;
+            } else if (hrac >= bot) { /* Vypise mensie cislo */
+                System.out.println("Cislo je mensie");
+                pocetPokus -= 1; /* Odcita pokus */
+                System.out.println("Mas este: " + pocetPokus + " pocet pokusov"); /* vypise pocet pokusov */
+                hrac = sc.nextInt(); /* Vyziada dalsie cislo */
+
+                if (pocetPokus > 0) {
+                    System.out.println("Cislo je mensie");
+                    pocetPokus -= 1;
+                    System.out.println("Mas este: " + pocetPokus + " pocet pokusov");
+                    hrac = sc.nextInt();
+                } else if (pocetPokus == 0) {
+                    System.out.println("Cislo je mensie");
+                    pocetPokus -= 1;
+                    System.out.println("Mas posledny pokus");
+                    hrac = sc.nextInt();
+                }
+            } else {
+                System.out.println("Cislo je vacsie");
+                pocetPokus -= 1;
+                System.out.println("Mas este: " + pocetPokus + " pocet pokusov");
+                hrac = sc.nextInt();
+                if (pocetPokus > 0) {
+                    System.out.println("Cislo je vacsie");
+                    pocetPokus -= 1;
+                    System.out.println("Mas este: " + pocetPokus + " pocet pokusov");
+                    hrac = sc.nextInt();
+                } else if (pocetPokus == 0) {
+                    System.out.println("Cislo je vacsie");
+                    pocetPokus -= 1;
+                    System.out.println("Mas posledny pokus");
+                    hrac = sc.nextInt();
+                }
+            }
+        }
+    }
+}
---
+++ HraHadajCislo/Main.java
@@ -0,0 +1,16 @@
+package HraHadajCislo;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Helper help = new Helper();
+        Random rand = new Random();
+
+        int hrac, bot;
+
+        bot = rand.nextInt(0, 10)+1;
+
+        help.hra(bot);
+    }
+}
