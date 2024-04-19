5b71ff9ce1687e759bf0d848b4d80b14627e5540
Molabe <153329619+Molabe@users.noreply.github.com>
2024-04-19 12:46:24 +0200

    .

--- HraHadajCislo/Helper.java
+++ HraHadajCislo/Helper.java
@@ -15,43 +15,22 @@ public class Helper {
 
         int pocetPokus = 3;
 
-        while (pocetPokus >= 0) {
+        while (pocetPokus >= 1) {
             if (hrac == bot) { /* Vyhlasi vyhru a program sa vypne */
                 System.out.println("Vyhral si");
                 break;
-            } else if (hrac > bot) { /* Vypise mensie cislo */
+            }
+            if (hrac >= bot) { /* Vypise mensie cislo */
                 System.out.println("Cislo je mensie");
-                pocetPokus -= 1; /* Odcita pokus */
+                pocetPokus--; /* Odcita pokus */
                 System.out.println("Mas este: " + pocetPokus + " pocet pokusov"); /* vypise pocet pokusov */
                 hrac = sc.nextInt(); /* Vyziada dalsie cislo */
 
-                if (pocetPokus > 0) {
-                    System.out.println("Cislo je mensie");
-                    pocetPokus -= 1;
-                    System.out.println("Mas este: " + pocetPokus + " pocet pokusov");
-                    hrac = sc.nextInt();
-                } else if (pocetPokus == 0) {
-                    System.out.println("Cislo je mensie");
-                    pocetPokus -= 1;
-                    System.out.println("Mas posledny pokus");
-                    hrac = sc.nextInt();
-                }
             } else {
                 System.out.println("Cislo je vacsie");
-                pocetPokus -= 1;
+                pocetPokus--;
                 System.out.println("Mas este: " + pocetPokus + " pocet pokusov");
                 hrac = sc.nextInt();
-                if (pocetPokus > 0) {
-                    System.out.println("Cislo je vacsie");
-                    pocetPokus -= 1;
-                    System.out.println("Mas este: " + pocetPokus + " pocet pokusov");
-                    hrac = sc.nextInt();
-                } else if (pocetPokus == 0) {
-                    System.out.println("Cislo je vacsie");
-                    pocetPokus -= 1;
-                    System.out.println("Mas posledny pokus");
-                    hrac = sc.nextInt();
-                }
             }
         }
     }
